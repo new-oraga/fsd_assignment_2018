f5460cdec97a56807952ea7b48578dd7195b630e
hst <genji0429@qq.com>
2018-06-16 13:43:34 +0800

    The mainMenu_colorful version must
    running on linux on like linux system or
    
    tools.

--- mainMenu_colorful_verson.py
+++ mainMenu_colorful_verson.py
@@ -14,19 +14,30 @@ timetable = ['* 1) 10:00 am', '2) 11:00 am', '3) 12:00 am', '4) 1:00 pm', '5) 2:
              '8) 5:00 pm']
 
 
+class fontcolors:
+    BULE = '\033[1;34m'
+    PINK = '\033[95m'
+    RED = '\033[1;31m'
+    GREEN = '\033[1;32m'
+    YELLOW = '\033[1;33m'
+    YRED = '\033[1;35m'
+    QGREEN = '\033[1;36m'
+    ENDC = '\033[0m'
+
+
 # exit
 def exit_sys():
-    print ' ' * 10, 'You have already exited this system!'
+    print ' ' * 10, fontcolors.BULE + 'You have already exited this system!' + fontcolors.ENDC
 
 
 def consequ():
-    print  ' ' * 10, '*' * 40
-    print  ' ' * 10, '* Ticket is purchased. Have a nice day *'
-    print  ' ' * 10, '* ==================================== *'
-    print ' ' * 10, '* Do you still want to continue?       *'
-    print ' ' * 10, '* M - to Main Menu                     *'
-    print ' ' * 10, '* Q - to Quit the system               *'
-    print  ' ' * 10, '*' * 40
+    print  ' ' * 10, fontcolors.YELLOW + '*' * 40 + fontcolors.YELLOW
+    print  ' ' * 10, fontcolors.YELLOW + '* Ticket is purchased. Have a nice day *' + fontcolors.YELLOW
+    print  ' ' * 10, fontcolors.YELLOW + '* ==================================== *' + fontcolors.YELLOW
+    print ' ' * 10, fontcolors.YELLOW + '* Do you still want to continue?       *' + fontcolors.YELLOW
+    print ' ' * 10, fontcolors.YELLOW + '* M - to Main Menu                     *' + fontcolors.YELLOW
+    print ' ' * 10, fontcolors.YELLOW + '* Q - to Quit the system               *' + fontcolors.YELLOW
+    print  ' ' * 10, fontcolors.YELLOW + '*' * 40
     print ' ' * 10,
     s = raw_input('Please select your option : ')
     print
@@ -41,17 +52,19 @@ def addition(s, dateFerry, date, ferryId, name, number):
             # judgement
             nnow = len(dateFerry[date][ferryId - 1][0])
             if number > 10:
-                print ' ' * 10, '* please buy the tickets no more than 10 '
+                print ' ' * 10, fontcolors.PINK, '* please buy the tickets no more than 10 '
+                print
                 return
             if (nnow + number) > 10 and number <= 10:
-                print ' ' * 10, '* the seat is full now, please buy tickets no more than %d ' % (10 - nnow)
-                print ' ' * 10,
+                print ' ' * 10, fontcolors.PINK, '* the seat is full now, please buy tickets no more than %d ' % (
+                            10 - nnow)
+                print ' ' * 10, fontcolors.PINK,
                 number = raw_input('* please input the number of tickets you want to buy : ')
                 print
             try:
                 number = int(number)
             except:
-                print ' ' * 10, '=== Invalid input === '
+                print ' ' * 10, fontcolors.RED, '=== Invalid input === '
                 print
                 return
 
@@ -72,7 +85,7 @@ def addition(s, dateFerry, date, ferryId, name, number):
 
         else:
 
-            print ' ' * 10, ('This ferry 00%d is full now!' % ferryId)
+            print ' ' * 10, fontcolors.PINK, ('This ferry 00%d is full now!' % ferryId)
 
             return 'fail'
     elif s == 'E':
@@ -81,18 +94,20 @@ def addition(s, dateFerry, date, ferryId, name, number):
             # judgement
             nnow = len(dateFerry[date][ferryId - 1][1])
             if number > 40:
-                print ' ' * 10, '* please buy the tickets no more than 40 '
+                print ' ' * 10, fontcolors.PINK, '* please buy the tickets no more than 40 '
+                print
                 return
             if (nnow + number) > 40 and number <= 40:
-                print ' ' * 10, '* the seat is full now, please buy tickets no more than %d ' % (40 - nnow)
-                print ' ' * 10,
+                print ' ' * 10, fontcolors.PINK, '* the seat is full now, please buy tickets no more than %d ' % (
+                            40 - nnow)
+                print ' ' * 10, fontcolors.PINK,
                 number = raw_input('* please input the number of tickets you want to buy : ')
                 print
 
             try:
                 number = int(number)
             except:
-                print ' ' * 10, '=== Invalid input === '
+                print ' ' * 10, fontcolors.RED, '=== Invalid input === '
                 print
                 return
 
@@ -113,7 +128,7 @@ def addition(s, dateFerry, date, ferryId, name, number):
 
         else:
 
-            print ' ' * 10, 'This ferry 00%d is full now!' % ferryId
+            print ' ' * 10, fontcolors.PINK, 'This ferry 00%d is full now!' % ferryId
             return 'fail'
 
 
@@ -129,26 +144,26 @@ def selectdate(s):
     try:
         comdate = int(comdate)
     except:
-        print ' ' * 10, '=== Invalid input === '
+        print ' ' * 10, fontcolors.RED, '=== Invalid input === '
         print
         print ' ' * 10,
         date = raw_input(' Please input the date (format must like this : \'2018-5-30\') : ')
         print
     # select ferryId
     ptimetable()
-    print ' ' * 10,
+    print ' ' * 10, fontcolors.PINK,
     ferryId = int(raw_input(' please select the timetable (select the number: 1, 2, 3 so on..) : '))
     print
-    print ' ' * 10,
+    print ' ' * 10, fontcolors.PINK,
     name = raw_input(' please input your name : ')
     print
-    print ' ' * 10,
+    print ' ' * 10, fontcolors.PINK,
     number = raw_input(' please input the number of tickets you want to buy : ')
     print
     try:
         number = int(number)
     except:
-        print ' ' * 10, '=== Invalid input === '
+        print ' ' * 10, fontcolors.RED, '=== Invalid input === '
         print
         return
 
@@ -169,7 +184,7 @@ def selectdate(s):
 # Submenu purchase ticket
 def purchase(BussinessList, EconomyList):
     ppurchase()
-    print ' ' * 10,
+    print ' ' * 10, fontcolors.GREEN,
     s = raw_input('Please select your option : ')
     print
     s = s.upper()
@@ -186,13 +201,13 @@ def purchase(BussinessList, EconomyList):
                 print(' ' * 10, 'You have already exited this system!')
                 exit()
             else:
-                print ' ' * 10, '=== Invalid input ==='
+                print ' ' * 10, fontcolors.RED, '=== Invalid input ==='
                 print
                 return
     elif s == 'M':
         return 'continue'
     else:
-        print ' ' * 10, '=== Invalid input ==='
+        print ' ' * 10, fontcolors.RED, '=== Invalid input ==='
         print
         return
 
@@ -200,7 +215,7 @@ def purchase(BussinessList, EconomyList):
 # Submenu View Seating Arrangement
 def view(BussinessList, EconomyList):
     pview()
-    print ' ' * 10,
+    print ' ' * 10, fontcolors.YELLOW,
     s = raw_input('Please select your choice : ')
     print
     s = s.upper()
@@ -215,42 +230,42 @@ def view(BussinessList, EconomyList):
             # print date list
             cout = 0
             for k in dateFerry:
-                cout+=1
-                if cout%6 ==0:
+                cout += 1
+                if cout % 6 == 0:
                     print
                     print ' ' * 10,
-                    print '*', k,
+                    print fontcolors.YELLOW + '*', fontcolors.YELLOW + k,
                 else:
-                    print '*', k,
+                    print fontcolors.YELLOW + '*', fontcolors.YELLOW + k,
             print
             print  ' ' * 10, '*' * 108
-            print ' ' * 10, '*',
+            print ' ' * 10, '*', fontcolors.YELLOW,
             date = raw_input(' Please input the date you want to select (the format must be the same as given) : ')
             print
 
-            print ' ' * 10,
+            print ' ' * 10, fontcolors.YELLOW,
             ferryId = int(raw_input("Please select your FerryId (range from 1 to 8) : "))
             print
 
         except:
 
-            print ' ' * 10, '=== Invalid input ==='
+            print ' ' * 10, fontcolors.RED, '=== Invalid input ==='
             print
             return
 
     else:
-        print ' ' * 10, '=== Invalid input ==='
+        print ' ' * 10, fontcolors.RED, '=== Invalid input ==='
         print
         return
 
     # printing part
-    print ' ' * 10,
+    print ' ' * 10, fontcolors.PINK,
     print ('********************************************************************')
-    print ' ' * 10,
+    print ' ' * 10, fontcolors.PINK,
     print ('*         Ferry ID : 00%d           Date : %s                *' % (ferryId, date))
-    print ' ' * 10,
+    print ' ' * 10, fontcolors.PINK,
     print ('********************************************************************')
-    print ' ' * 10,
+    print ' ' * 10, fontcolors.PINK,
     print ('*         Bussiness CLASS                                          *')
     # Algorithm 1 : bussiness seat
     if s == 'T':
@@ -263,17 +278,17 @@ def view(BussinessList, EconomyList):
         for i in range(1, cha + 1):
             printList.append(0)
     print ' ' * 10,
-    print '* ',
+    print fontcolors.PINK, '* ',
     for i in range(10):
         if i == 5:
             print
-            print ' ' * 10, '* ',
-        print printList[i], ' * ',
+            print ' ' * 10, fontcolors.PINK, '* ',
+        print fontcolors.PINK, printList[i], fontcolors.PINK, ' * ',
     print
 
     del printList[:]
 
-    print ' ' * 10,
+    print ' ' * 10, fontcolors.PINK,
     print ('*         Economy CLASS                                            *')
 
     # Algorithm 2 economy seat
@@ -288,12 +303,12 @@ def view(BussinessList, EconomyList):
             printList.append(0)
 
     print ' ' * 10,
-    print '* ',
+    print fontcolors.PINK, '* ',
     for i in range(40):
         if i == 5 or i == 10 or i == 15 or i == 20 or i == 25 or i == 30 or i == 35:
             print
-            print ' ' * 10, '* ',
-        print printList[i], ' * ',
+            print ' ' * 10, fontcolors.PINK, '* ',
+        print fontcolors.PINK, printList[i], fontcolors.PINK, ' * ',
     print
     print
 
@@ -301,73 +316,73 @@ def view(BussinessList, EconomyList):
 
 
 def pmian():
-    print  ' ' * 10, '*' * 36
-    print  ' ' * 10, '* FERRY TICKETING SYSTEM           *'
-    print  ' ' * 10, '* P – to Purchase Ticket           *'
-    print  ' ' * 10, '* V – to View Seating Arrangement  *'
-    print  ' ' * 10, '* Q – to Quit the system           *'
-    print  ' ' * 10, '* please select your option        *'
-    print  ' ' * 10, '*' * 36
+    print  ' ' * 10, fontcolors.GREEN + '*' * 36 + fontcolors.ENDC
+    print  ' ' * 10, fontcolors.GREEN + '* FERRY TICKETING SYSTEM           *' + fontcolors.ENDC
+    print  ' ' * 10, fontcolors.GREEN + '* P – to Purchase Ticket           *' + fontcolors.ENDC
+    print  ' ' * 10, fontcolors.GREEN + '* V – to View Seating Arrangement  *' + fontcolors.ENDC
+    print  ' ' * 10, fontcolors.GREEN + '* Q – to Quit the system           *' + fontcolors.ENDC
+    print  ' ' * 10, fontcolors.GREEN + '* please select your option        *' + fontcolors.ENDC
+    print  ' ' * 10, fontcolors.GREEN + '*' * 36 + fontcolors.ENDC
 
 
 def ppurchase():
-    print  ' ' * 10, '*' * 45
-    print ' ' * 10, '* PURCHASING MODULE                         *'
-    print ' ' * 10, '* B – to purchase ticket for Business class *'
-    print ' ' * 10, '* E – to purchase ticket for Economy class  *'
-    print ' ' * 10, '* M – to return to Main Menu                *'
-    print  ' ' * 10, '*' * 45
+    print  ' ' * 10, fontcolors.GREEN + '*' * 45 + fontcolors.ENDC
+    print ' ' * 10, fontcolors.GREEN + '* PURCHASING MODULE                         *' + fontcolors.ENDC
+    print ' ' * 10, fontcolors.GREEN + '* B – to purchase ticket for Business class *' + fontcolors.ENDC
+    print ' ' * 10, fontcolors.GREEN + '* E – to purchase ticket for Economy class  *' + fontcolors.ENDC
+    print ' ' * 10, fontcolors.GREEN + '* M – to return to Main Menu                *' + fontcolors.ENDC
+    print  ' ' * 10, fontcolors.GREEN + '*' * 45 + fontcolors.ENDC
 
 
 def pview():
-    print  ' ' * 10, '*' * 30
-    print ' ' * 10, '* SEATING ARRANGEMENT MODULE *'
-    print ' ' * 10, '* T - to select Trip Time    *'
-    print ' ' * 10, '* M - to Main Menu           *'
-    print  ' ' * 10, '*' * 30
+    print  ' ' * 10, fontcolors.YELLOW + '*' * 30 + fontcolors.ENDC
+    print ' ' * 10, fontcolors.YELLOW + '* SEATING ARRANGEMENT MODULE *' + fontcolors.ENDC
+    print ' ' * 10, fontcolors.YELLOW + '* T - to select Trip Time    *' + fontcolors.ENDC
+    print ' ' * 10, fontcolors.YELLOW + '* M - to Main Menu           *' + fontcolors.ENDC
+    print  ' ' * 10, fontcolors.YELLOW + '*' * 30 + fontcolors.ENDC
 
 
 def ptrapezium():
     l = [10, 12, 14]
     for i in l:
-        print ' ' * 10, ' ' * (10 - (i - 10) / 2), '*' * i
+        print ' ' * 10, ' ' * (10 - (i - 10) / 2), fontcolors.BULE + '*' * i + fontcolors.ENDC
         # time.sleep(0.3)
     l1 = [28, 26, 24, 22]
     for i in l1:
-        print ' ' * 10, ' ' * (10 - (i - 10) / 2), '*' * i
+        print ' ' * 10, ' ' * (10 - (i - 10) / 2), fontcolors.BULE + '*' * i + fontcolors.ENDC
         time.sleep(0.3)
 
 
 def ptimetable():
-    print  ' ' * 10, '*' * 108
+    print  ' ' * 10, fontcolors.PINK + '*' * 108 + fontcolors.ENDC
     print ' ' * 10,
     for t in timetable:
-        print t, '*',
+        print fontcolors.PINK + t, fontcolors.PINK + '*' + fontcolors.ENDC,
     print
-    print  ' ' * 10, '*' * 108
+    print  ' ' * 10, fontcolors.PINK + '*' * 108 + fontcolors.ENDC
     print
 
 
 def pAllpersondetails(s, BussinessList, EconomyList):
     print
-    print  ' ' * 10, '*' * 52
+    print  ' ' * 10, fontcolors.PINK + '*' * 52 + fontcolors.ENDC
     if s == 'B':
         for b in BussinessList:
-            print  ' ' * 10, '* ', 'Name: ', b.name
-            print  ' ' * 10, '* ', 'SeatType: ', b.type
-            print  ' ' * 10, '* ', 'FerryId: ', b.ferryId
-            print  ' ' * 10, '* ', 'Fate: ', b.date
-            print  ' ' * 10, '* ', 'TicketsNumber: ', b.number
+            print  ' ' * 10, fontcolors.PINK + '* ', 'Name: ', b.name + fontcolors.ENDC
+            print  ' ' * 10, fontcolors.PINK + '* ', 'SeatType: ', b.type + fontcolors.ENDC
+            print  ' ' * 10, fontcolors.PINK + '* ', 'FerryId: ', str(b.ferryId) + fontcolors.ENDC
+            print  ' ' * 10, fontcolors.PINK + '* ', 'Fate: ', b.date + fontcolors.ENDC
+            print  ' ' * 10, fontcolors.PINK + '* ', 'TicketsNumber: ', str(b.number) + fontcolors.ENDC
             print
     elif s == 'E':
         for e in EconomyList:
-            print  ' ' * 10, '* ', 'Name: ', e.name
-            print  ' ' * 10, '* ', 'SeatType: ', e.type
-            print  ' ' * 10, '* ', 'FerryId: ', e.ferryId
-            print  ' ' * 10, '* ', 'Fate: ', e.date
-            print  ' ' * 10, '* ', 'TicketsNumber: ', e.number
+            print  ' ' * 10, fontcolors.PINK + '* ', 'Name: ', e.name + fontcolors.ENDC
+            print  ' ' * 10, fontcolors.PINK + '* ', 'SeatType: ', e.type + fontcolors.ENDC
+            print  ' ' * 10, fontcolors.PINK + '* ', 'FerryId: ', str(e.ferryId) + fontcolors.ENDC
+            print  ' ' * 10, fontcolors.PINK + '* ', 'Fate: ', e.date + fontcolors.ENDC
+            print  ' ' * 10, fontcolors.PINK + '* ', 'TicketsNumber: ', str(e.number) + fontcolors.ENDC
             print
-    print  ' ' * 10, '*' * 52
+    print  ' ' * 10, fontcolors.PINK + '*' * 52 + fontcolors.ENDC
     print
 
 
@@ -377,7 +392,7 @@ if __name__ == '__main__':
         ptrapezium()
         print
         pmian()
-        print ' ' * 10,
+        print ' ' * 10, fontcolors.GREEN,
         s = raw_input('* Your option :      ')
         print
         s = s.upper()
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 from Person import Person
 from PersonDetails import PersonDetails
 
@@ -26,17 +26,79 @@ from PersonDetails import PersonDetails
 #     print('0%d'%(i))
 #
 # print '\n\n'
-
-l = [1,1,1,1,1]
-del l[:]
-print l
-l.append(1)
-l.append(1)
-l.append(1)
-print(l)
+#
+# l = [1,1,1,1,1]
+# del l[:]
+# print l
+# l.append(1)
+# l.append(1)
+# l.append(1)
+# print(l)
 
 
 # ferryid = FerryId('001')
 # print ferryid.id
 # person = Person('name','bussiness')
 # print person.name,person.type
+
+# print 'aaaaaaaa',
+# print
+# print 'aaaaaaaaaaa'
+#
+#
+# l = [1,2,3]
+# print len(l)+1
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+print bcolors.WARNING + "Warning: No active frommets remain. Continue?" + bcolors.ENDC
+
+
+CRED = '\033[91m'
+CEND = '\033[0m'
+print(CRED + "Error, does not compute!" + CEND)
+
+#! /usr/bin/python                                                          
+# -*- coding: utf-8
+ 
+STYLE = {
+        'fore': {
+                'black': 30, 'red': 31, 'green': 32, 'yellow': 33,
+                'blue': 34, 'purple': 35, 'cyan': 36, 'white': 37,
+        },
+        'back': {
+                'black': 40, 'red': 41, 'green': 42, 'yellow': 43,
+                'blue': 44, 'purple': 45, 'cyan': 46, 'white': 47,
+        },
+        'mode': {
+                'bold': 1, 'underline': 4, 'blink': 5, 'invert': 7,
+        },
+        'default': {
+                'end': 0,
+        }
+}
+ 
+def use_style(string, mode='', fore='', back=''):
+    mode = '%s' % STYLE['mode'][mode] if STYLE['mode'].has_key(mode) else ''
+    fore = '%s' % STYLE['fore'][fore] if STYLE['fore'].has_key(fore) else ''
+    back = '%s' % STYLE['back'][back] if STYLE['back'].has_key(back) else ''
+    style = ';'.join([s for s in [mode, fore, back] if s])
+    style = '\033[%sm' % style if style else ''
+    end = '\033[%sm' % STYLE['default']['end'] if style else ''
+    return '%s%s%s' % (style, string, end)
+ 
+def test():
+    print use_style('Normal')
+    print use_style('Bold', mode='bold')
+    print use_style('Underline & red text', mode='underline', fore='red')
+    print use_style('Invert & green back', mode='reverse', back='green')
+    print use_style('Black text & White back', fore='black', back='white')
+ 
+if __name__ == '__main__':
+    test()
